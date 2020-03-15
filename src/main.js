b17895115d1ea4a6409f5ca189e79a14c6288f52
impulseDuck <qizi542261886@gmail.com>
2020-03-15 11:08:17 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'// 引入vant组件库
+import 'vant/lib/index.css'// 引入vant组件的样式
+Vue.use(Vant) // 注册vant的组件库
+// 以上做法完整导入
 Vue.config.productionTip = false
 
 new Vue({
